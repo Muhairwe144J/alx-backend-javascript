938d997fddeda13424f66a460c30401d1b7a1c60
Muhairwe144J <mnasasirashafik144j@gmail.com>
2024-08-07 15:15:27 +0300

    promises

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,25 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}}
