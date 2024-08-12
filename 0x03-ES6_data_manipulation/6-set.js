0e4084a54dedb32bed0d62da34e37168172af606
Muhairwe144J <mnasasirashafik144j@gmail.com>
2024-08-12 21:43:56 +0300

    data manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Nasasira Shafik Muhairwe <https://github.com/Muhairwe144J>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
