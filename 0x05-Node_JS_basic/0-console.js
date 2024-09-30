e177d8e6af38f00bf7758a32b8de3dbffdc48336
Muhairwe144J <mnasasirashafik144j@gmail.com>
2024-10-01 00:10:41 +0300

    node js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ *  @author Nasasira Shafik Muhairwe <https://github.com/Muhairwe144J>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
